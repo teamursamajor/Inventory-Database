a0a7ae3f7c9332fc52713f742ac4c875cd0f2f27
Charlie Schneider <charlies1999@gmail.com>
2016-02-27 15:24:04 -0500

    added '-g' or 'get item' option, '-e' or 'edit item' function is a WIP

--- inventory.py
+++ inventory.py
@@ -1,11 +1,16 @@
 import sys
 import os
 
-def createDatabase(filename):
-	return open(filename, 'w+')
-
 def openDatabase(filename, mode):
 	return open(filename, mode)
+
+def closeDatabase(filename):
+	try:
+		openDatabase(filename, 'r').close()
+	except IOError:
+		print("There was an error closing the database")
+		pass
+	
 
 def deleteDatabase(filename):
 	try:
@@ -15,55 +20,67 @@ def deleteDatabase(filename):
 
 def wipeDatabase(database):
 	database.close()
-	return createDatabase(database.name)
+	return openDatabase(database.name, "w+")
 
 def readDatabase(database):
 	database.seek(0)
 	return database.read()
 
+def inDatabase(database, item):
+	data = readDatabase(database)
+	if data.find(item[0]) == -1:
+		return False
+	else: 
+		return True
+
 def databaseToDict(database):
-	items = []
-	stocks = {}
-	locations = {}
+	items = {}
 	tok = ""
 	listing = [] 
 	for char in readDatabase(database):
-		if char != ';':
+		if char != '\n':
 			tok += char
 		else:
 			listing = tok.split(':')
-			items.append(listing[0])
-			stocks[listing[0]] = listing[1]
-			locations[listing[0]] = listing[2]
+			items[listing[0]] = [listing[0], listing[1], listing[2]]
+			listing = []
 			tok = ""
-	return items, stocks, locations
+	return items
 
-def createItem(database, item, stock, location):
-	if readDatabase(database).find(item) == -1:
-		database.write("%s:%s:%s;" % (item, stock, location))
+def createItem(database, item):
+	if not inDatabase(database, item):
+		database.write("%s:%s:%s" % (item[0], item[1], item[2]))
 	else:
-		print("Item %s already exits" % (item))
+		print("Item %s already exits" % (item[0]))
 
 def removeItem(database, item):
 	data = readDatabase(database)
-	itemIndex = data.find(item)
+	itemIndex = data.find(item[0])
 	if itemIndex == -1:
-		print("Item %s not found" % (item))
+		print("Item %s not found" % (item[0]))
 		sys.exit()
-	nextSemi = data.find(';', itemIndex)
+	nextNewline = data.find('\n', itemIndex, len(data))
 	database = wipeDatabase(database)
-	database.write(data[:itemIndex] + data[nextSemi+1:])
+	database.write(data[:itemIndex] + data[nextNewline+1:])
+
+
+
+def printItem(database, item):
+	if inDatabase(database, item[0]):
+		print("Item: %s Stock: %s Location: %s" % (item[0], item[1], item[2]))
+	else:
+		print("Item %s not found" % (item[0]))
 
 def listItems(database):
-	items, stocks, locations = databaseToDict(database)
+	items = databaseToDict(database)
 	for item in items:
-		print("Item: %s Stock: %s Location: %s" % (item, stocks[item], locations[item]))
+		printItem(database, items[item])
 
 def parse(args):
 	for arg in args:
 		if arg == "-c":
 			if len(args) == 2:
-				createDatabase(args[1])
+				openDatabase(args[1], 'w+')
 			else:
 				print("ERROR: inv -c [filename]")
 		elif arg == "-d":
@@ -74,7 +91,7 @@ def parse(args):
 		elif arg == "-i":
 			if len(args) == 5:
 				database = openDatabase(args[1], 'r+')
-				createItem(database, args[2], args[3], args[4])
+				createItem(database, [args[2], args[3], args[4]])
 			else:
 				print("ERROR: inv -i [database] [item] [stock] [location]")
 		elif arg == "-r":
@@ -89,5 +106,19 @@ def parse(args):
 				listItems(database)
 			else:
 				print("ERROR: inv -l [database]")
+		elif arg == "-g":
+			if len(args) == 3:
+				database = openDatabase(args[1], 'r')
+				items = databaseToDict(database)
+				printItem(database, items[args[2]])
+			else:
+				print("ERROR: inv -g [database] [item]")
+		elif arg == "-e":
+			if len(args) == 5:
+				database = openDatabase(args[1], 'r')
+				items = databaseToDict(database)
+				if args[3] == "stock":
+					items[args[2]] = str(int(items[args[2]][1]) + args[4])
+	closeDatabase(args[1])
 
 parse(sys.argv[1:])
